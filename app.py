e6966323497d6018dd69603537ac5f9d68151a2d
kianhean <kianhean@gmail.com>
2017-11-12 13:20:07 +0800

    flask + telepot framework migration!

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import telepot
 
 CHANNEL_ID = '-1001311613849'
 
@@ -7,34 +8,13 @@ CHANNEL_ID = '-1001311613849'
 TOKEN = os.environ.get('TELEGRAM_TOKEN')
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
-def receive_message(msg):
-    """Receive a raw message from Telegram"""
-    try:
-        message = str(msg["message"]["text"])
-        chat_id = msg["message"]["chat"]["id"]
-        return message, chat_id
-    except Exception as e:
-        print(e)
-        return (None, None)
+bot = telepot.Bot(TOKEN)
 
-def handle_message(message):
-    """Calculate a response to the message"""
-    return message
 
-def send_message(message, chat_id):
-    """Send a message to the Telegram chat defined by chat_id"""
-    data = {"text": message.encode("utf8"), "chat_id": chat_id}
-    url = BASE_URL + "/sendMessage"
-    try:
-        response = requests.post(url, data).content
-    except Exception as e:
-        print(e)
+def handle(message):
+    message = message['message']
+    content_type, chat_type, chat_id = telepot.glance(message)
+    print(content_type, chat_type, chat_id)
 
-def run(message):
-    """Receive a message, handle it, and send a response"""
-    try:
-        message, chat_id = receive_message(message)
-        response = handle_message(message)
-        send_message(response, chat_id)
-    except Exception as e:
-        print(e)
+    if content_type == 'text':
+        bot.sendMessage(chat_id, message['text'])
--- bot_server.py
+++ bot_server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import request
-from app import run
+from app import handle
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def receive():
     try:
-        run(request.json)
+        print(request.json)
+        handle(request.json)
         return ""
     except Exception as e:
         print(e)
